0b19f807fccf47d623f2c4571ee7eacff19532d4
Dave Best <dave.e.best@gmail.com>
2022-09-05 10:17:06 -0400

    More tweaks for app deployment.

--- npverify/settings.py
+++ npverify/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False") == "True"
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", ['127.0.0.1','localhost'])
 
 
 # Application definition
