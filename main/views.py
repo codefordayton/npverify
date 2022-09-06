89a33e83950ddcc26f152da29abf2ad9273371c5
Dave Best <dave.e.best@gmail.com>
2022-09-05 21:28:05 -0400

    Yay, bootstrap. :grimace:

--- main/views.py
+++ main/views.py
@@ -5,27 +5,31 @@ from django.contrib import messages
 
 from .models import Nonprofit
 
-# Create your views here.
 def get_nonprofit():
     return Nonprofit.objects.all().filter(complete=False)[:1][0]
+
+def none_check(value):
+    return None if value == 'None' else value
 
 def index(request):
     context = {'nonprofit': get_nonprofit()}
     return render(request, 'main/index.html', context)
 
 def detail(request, nonprofit_id):
-    context = {'nonprofit': get_nonprofit()}
+    nonprofit = get_object_or_404(Nonprofit, pk=nonprofit_id)
+    context = {'nonprofit': nonprofit}
     return render(request, 'main/detail.html', context)
 
 def review(request, nonprofit_id):
     nonprofit = get_object_or_404(Nonprofit, pk=nonprofit_id)
-    nonprofit.website_url = request.POST['website_url']
-    nonprofit.facebook_url = request.POST['facebook_url']
+    nonprofit.website_url = none_check(request.POST['website_url'])
+    nonprofit.facebook_url = none_check(request.POST['facebook_url'])
     nonprofit.needs_volunteers = request.POST.get('needs_volunteers', False) == 'on'
     nonprofit.needs_review = request.POST.get('needs_review', False) == 'on'
-    nonprofit.review_comment = request.POST['review_comment']
+    nonprofit.review_comment = none_check(request.POST['review_comment'])
     nonprofit.complete = True
     nonprofit.save()
     messages.success(request, 'Review submitted. Thank you!')
+    print(nonprofit.facebook_url)
     next_nonprofit = get_nonprofit()
     return HttpResponseRedirect(reverse('detail', args=(next_nonprofit.id,)))
