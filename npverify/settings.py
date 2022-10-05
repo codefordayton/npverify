3b791244971d5006ef0c741c674cef3e09b4d172
Paul Schmidt <paulschmidt@Paul-Schmidt.local>
2022-10-04 20:01:49 -0400

    completing step 6

--- npverify/settings.py
+++ npverify/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme' # Tailwind styling app directory
 ]
 
 MIDDLEWARE = [
@@ -135,3 +137,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# https://django-tailwind.readthedocs.io/en/latest/installation.html
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
