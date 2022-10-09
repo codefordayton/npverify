bae53f3000ad0e1574df432fcbebd3f5962d10fc
Paul Schmidt <paulschmidt@paul-schmidt.lan>
2022-10-09 17:02:31 -0500

    remove django_browser_reload since it ain't a registered namespace

--- npverify/settings.py
+++ npverify/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     # Tailwind required apps
     'tailwind',
     'theme',
-    'django_browser_reload'
 ]
 
 MIDDLEWARE = [
@@ -59,7 +58,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'npverify.urls'
