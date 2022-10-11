12cbb85633b0750ae825b9847e947bc44b8616d4
Paul Schmidt <paulschmidt@Paul-Schmidt.local>
2022-10-11 15:16:13 -0500

    remove urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    path('', TemplateView.as_view(template_name="base.html")),     # new
-    path('admin/', admin.site.urls),
-]
